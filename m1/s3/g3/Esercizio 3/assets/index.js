248afe0c961ce379a8c962edcb466c583f59f71d
mcpierluigi <pierlui-gi@hotmail.com>
2023-02-15 17:23:23 +0100

    Esercizio 3

---
+++ m1/s3/g3/Esercizio 3/assets/index.js
@@ -0,0 +1,16 @@
+function creaTask(){
+    let area = document.querySelector('#testo');
+    let task = document.querySelector('#container');
+    
+    if(area.valure !== ''){
+    let nuovaTask = document.createElement('div');
+    let elimina = document.createElement('button');
+    elimina.className = 'pulsante';
+    elimina,addEventListener('click',() => nuovaTask.remove());
+    nuovaTask.addEventListener('click',() => nuovaTask.style.textDecoration = 'line-through');
+    nuovaTask.append(area.value);
+    nuovaTask.append(elimina);
+    task.append(nuovaTask);
+    area.value = '';
+    }
+}
