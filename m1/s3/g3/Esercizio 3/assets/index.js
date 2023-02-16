bf2f39857d7b2af1ce40459ecfec1be24bcb0f96
mcpierluigi <pierlui-gi@hotmail.com>
2023-02-16 17:17:01 +0100

    Es 4

--- m1/s3/g3/Esercizio 3/assets/index.js
+++ m1/s3/g3/Esercizio 3/assets/index.js
@@ -1,16 +1,19 @@
-function creaTask(){
-    let area = document.querySelector('#testo');
+function creaTasks(){
+    let area = document.querySelector("#testo");
     let task = document.querySelector('#container');
-    
-    if(area.valure !== ''){
+
+    if(area.value != ""){
     let nuovaTask = document.createElement('div');
     let elimina = document.createElement('button');
-    elimina.className = 'pulsante';
-    elimina,addEventListener('click',() => nuovaTask.remove());
-    nuovaTask.addEventListener('click',() => nuovaTask.style.textDecoration = 'line-through');
-    nuovaTask.append(area.value);
-    nuovaTask.append(elimina);
-    task.append(nuovaTask);
-    area.value = '';
+
+    elimina.className = "pulsante";
+    elimina.addEventListener("click", () => nuovaTask.remove());
+    nuovaTask.addEventListener("click", () => nuovaTask.style.textDecoration = "line-through");
+
+    nuovaTask.append(area.value)
+    nuovaTask.append(elimina)
+    task.append(nuovaTask)
+    area.value = ''
     }
 }
+
